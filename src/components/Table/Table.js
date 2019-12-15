070fa1c3c13f03b8c18991582ddbd02398c5ed03
Yuriy Borys <iam618384@gmail.com>
2019-12-15 18:17:31 +0200

    Update commit

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -8,26 +8,26 @@ const Table = (props) => {
 
     return (
         <table>
-            <tr>
+            <tr id="trow-0">
                 <th></th>
                 <th>Previous</th>
                 <th>Current</th>
                 <th>Delta</th>
             </tr>
-            <tr>
-                <th>USD</th>
+            <tr id="trow-1">
+                <th>$</th>
                 <td>{props.previous.USD}</td>
                 <td>{props.current.USD}</td>
                 <td>{countDelta(props.previous.USD, props.current.USD)}</td>
             </tr>
-            <tr>
-                <th>EUR</th>
+            <tr id="trow-2">
+                <th>€</th>
                 <td>{props.previous.EUR}</td>
                 <td>{props.current.EUR}</td>
                 <td>{countDelta(props.previous.EUR, props.current.EUR)}</td>
             </tr>
-            <tr>
-                <th>GBP</th>
+            <tr id="trow-3">
+                <th>£</th>
                 <td>{props.previous.GBP}</td>
                 <td>{props.current.GBP}</td>
                 <td>{countDelta(props.previous.GBP, props.current.GBP)}</td>
